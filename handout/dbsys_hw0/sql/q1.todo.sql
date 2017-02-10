496de42e2706550be6818e5028ec2f15f57f14f6
Nav Thakur <navthakur@Navs-MacBook-Pro.local>
2017-02-10 15:59:03 -0500

    added commments on q1 that might work better

--- handout/dbsys_hw0/sql/q1.todo.sql
+++ handout/dbsys_hw0/sql/q1.todo.sql
@@ -1,12 +1,24 @@
--- Find the top 10 parts that with the highest quantity in returned orders. 
+-- Find the top 10 parts that with the highest quantity in returned orders.
 -- An order is returned if the returnflag field on any lineitem part is the character R.
 -- Output schema: (part key, part name, quantity returned)
 -- Order by: by quantity returned, descending.
 
 -- Student SQL code here:
 SELECT p_partkey, p_name, LINEITEM.l_quantity
-FROM PART 
+FROM PART
 WHERE p_partkey = LINEITEM.l_partkey
 ORDER BY LINEITEM.l_quantity DESC
 LIMIT 10;
 
+/*
+SELECT p_partkey, p_name
+FROM part
+UNION
+SELECT l_partkey, l_quantity
+FROM lineitem
+WHERE p_partkey = l_partkey
+ORDER BY l_quantity DESC
+LIMIT 10;
+
+
+*/
